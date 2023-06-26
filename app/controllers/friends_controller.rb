7ef31fe8325bdd69364241e1cd91b5f6c2d739c1
OakleyJacob <jboyoakley@gmail.com>
2023-06-26 17:31:10 -0500

    added crud functionality

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,15 +1,28 @@
 class FriendsController < ApplicationController
 
     def index
+        friends = Friend.all
+        render json: friends
     end
   
     def create
+        friend = Friend.create(friend_params)
+        render json: friend
     end
   
     def update
+        friend = Friend.find(params[:id])
+        friend.update(friend_params)
+        render json: friend
     end
   
     def destroy
+        friend = Friend.find(params[:id])
+        friend.delete
+        render json: friend
     end
-
+    private
+    def friend_params
+        params.require(:friend).permit(:name, :img, :diet, :size, :species, :activities, :personality, :age)
+    end
 end
